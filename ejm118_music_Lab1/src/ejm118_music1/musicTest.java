f6f6585b8b9ab35d10fd099ab8ce82213888b041
ethanMackowick1239 <ethanmackowick@gmail.com>
2019-02-19 20:07:02 -0500

    Music_Lab1

---
+++ ejm118_music_Lab1/src/ejm118_music1/musicTest.java
@@ -0,0 +1,19 @@
+package ejm118_music1;
+import java.sql.*;
+import java.net.URL;
+public class musicTest {
+
+	public static void main(String[] args) 
+	{
+		
+		//Song song = new Song("baracuda", "2011-04-25 00:00:00", "2011-04-25 00:00:00");
+//		
+//		song.deleteSong("84c401ca-9d2d-495f-b821-5c9e14c36172");
+		//song.setLength(8);
+		//Artist artist = new Artist("Robert", "Plant", "Led Zeppelin");
+//		Artist artist = new Artist("a3903ccfebe743b998100635aaa34416");
+//		artist.deletArtist("a3903ccfebe743b998100635aaa34416");
+		
+		Album album = new Album("Led Zeppelin II", "2001-03-23 00:00:00", "Atlantic", 10, "explicit", 50);
+	}
+}
